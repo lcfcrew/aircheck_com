3e5905356103886484f46af508815938d256d87d
Chris Del Guercio <cdelguercio@gmail.com>
2016-04-23 12:17:19 -0700

    added static files route

--- aircheck_com/urls.py
+++ aircheck_com/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 import slothauth.urls as slothauth_urls
@@ -33,3 +34,5 @@ urlpatterns = [
 urlpatterns += [
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
 ]
+
+urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
