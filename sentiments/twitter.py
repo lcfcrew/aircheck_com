0a561cecb2b9403bfbbd18072adb9c6db483f064
Kobnar <konrad.rk.ludwig@gmail.com>
2016-04-23 16:29:16 -0700

    Pythonize tweet serialization

--- sentiments/twitter.py
+++ sentiments/twitter.py
@@ -2,35 +2,36 @@ import tweepy
 
 
 class TwitterAPI(object):
-    def __init__(self, consumer_token, consumer_secret, access_token,
-                 access_secret):
+    def __init__(self,
+                 query,
+                 consumer_token, consumer_secret,
+                 access_token, access_secret):
         auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
         auth.set_access_token(access_token, access_secret)
+        self.query = query
         self._api = tweepy.API(auth)
 
-    def search(self, query, max_items=1000):
-        """
-        Searches Twitter for tweets matching the given query. Results are
-        placed in a dictionary using the tweet id as the key.
+    def retrieve(self, max_items=100):
+        cursor = tweepy.Cursor(self._api.search, q=self.query).items(max_items)
+        return [self._serialize_tweet(t) for t in cursor]
 
-        :param query: A search string
-        :param max_items: The maximum number of tweets to return
-        :return: A dictionary of tweets (by "id")
-        """
-        cursor = tweepy.Cursor(self._api.search, q=query).items(max_items)
-        tweets = {}
-        for tweet in cursor:
-            tweets[tweet.id_str] = {
-                'id': tweet.id_str,
-                'created': tweet.created_at,
-                'location': tweet.coordinates,
-                'place': None,
-                'text': tweet.text,
-                'user': {
-                    'id': tweet.user.id,
-                    'screen_name': tweet.user.screen_name,
-                    'name': tweet.user.name,
-                    'location': tweet.user.location,
-                },
-            }
-        return tweets
+    def retrieve_new(self, tweet_id, max_items=100):
+        cursor = tweepy.Cursor(
+            self._api.search, q=self.query, since_id=tweet_id).items(max_items)
+        return [self._serialize_tweet(t) for t in cursor]
+
+    @staticmethod
+    def _serialize_tweet(tweet):
+        return {
+            'id': tweet.id_str,
+            'created': tweet.created_at,
+            'location': tweet.coordinates,
+            'place': None,
+            'text': tweet.text,
+            'user': {
+                'id': tweet.user.id,
+                'screen_name': tweet.user.screen_name,
+                'name': tweet.user.name,
+                'location': tweet.user.location,
+            },
+        }
