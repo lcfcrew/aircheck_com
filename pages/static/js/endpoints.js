0015010f71d9051e780535a1929de94142357a4b
KyleSpitznagel <kylespitz@gmail.com>
2016-04-23 13:35:04 -0700

    Initialized endpoint.js

---
+++ pages/static/js/endpoints.js
@@ -0,0 +1,31 @@
+
+// returns a list of latest 3 tweets
+function getSentiment(var NumOfSentiments, var )
+{
+return { "I love breathing so, so much...", }
+}
+
+function postSentiment(var string, var geoIP)
+{
+return { // id or key of the twitter post and the lat, long so it can orbit there}
+}
+
+// Gets a GeoIP as input from server and returns the lat and long
+function getLatLongFromGeoIP(var geoIP)
+{
+return {33.000, -118.000};
+}
+
+function getLatLongFromString(var location) // var is a string like "Los Angeles", gets a location back
+{
+return {34.000, -118.000};
+}
+
+
+
+// CHRIS VIA DJANGO BACKEND
+function getAirAnalysis( var lat, var long)
+{
+return //a bunch of data in JSON
+}
+
