f1f44ce1db2d82d101e4e27323bbd20c0ffddc3c
Kobnar <konrad.rk.ludwig@gmail.com>
2016-04-23 17:53:21 -0700

    Update url routing

--- sentiments/urls.py
+++ sentiments/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^api/' + settings.API_VERSION + '/sentiments/', views.sentiment_collection),
+    url(r'^api/' + settings.API_VERSION + '/sentiments/twitter/', views.twitter_endpoint),
+    url(r'^api/' + settings.API_VERSION + '/sentiments/', views.sentiment_endpoint),
 ]
