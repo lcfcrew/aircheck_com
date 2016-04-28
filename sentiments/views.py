56dda28e392c989d96202660e6f58d2f86595e3e
Kobnar <konrad.rk.ludwig@gmail.com>
2016-04-28 14:08:58 -0700

    Strip new tweet fetching and analysis calls from REST API

--- sentiments/views.py
+++ sentiments/views.py
@@ -9,8 +9,6 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 
 from . import models
-from . import twitter
-from . import azure
 from . import _DEFAULT_MAX_ITEMS
 
 
@@ -37,29 +35,6 @@ class JSONResponse(HttpResponse):
         super(JSONResponse, self).__init__(content, **kwargs)
 
 
-def _do_analysis():
-    sentiments = models.Sentiment.objects.filter(sentiment__isnull=True)
-    azure_api = azure.AzureAPI()
-    azure_data = {
-        'documents': [{'id': s.id, 'text': s.text} for s in sentiments]
-    }
-    sentiment_scores = azure_api.sentiment(azure_data)
-    key_phrases = azure_api.key_phrases(azure_data)
-
-    for idx, sentiment in enumerate(sentiments):
-        sentiment_score = sentiment_scores['documents'][idx]['score']
-        key_phrase_list = key_phrases['documents'][idx]['keyPhrases']
-
-        sentiment.is_tweet = True
-        sentiment.sentiment = sentiment_score
-        sentiment.save()
-
-        for phrase in key_phrase_list:
-            phrase_obj = models.KeyPhrase.objects.create(
-                sentiment=sentiment, phrase=phrase)
-            phrase_obj.save()
-
-
 @csrf_exempt
 def sentiments_endpoint(request):
     """
@@ -83,11 +58,6 @@ def sentiments_endpoint(request):
 
     elif request.method == 'GET':
         max_items = request.GET.get('max_items') or 100
-        do_analyze = request.GET.get('analyze') or False
-
-        if do_analyze:
-            _do_analysis()
-
         sentiments = models.Sentiment.objects.filter(latitude__isnull=False)[:max_items]
         serializer = models.SentimentSerializer(sentiments, many=True)
         return JSONResponse(serializer.data)
@@ -108,39 +78,4 @@ def tweets(request):
             return JSONResponse(serializer.data)
         except ObjectDoesNotExist:
             return JSONResponse([])
-    return JSONResponse([], status=400)
-
-
-@csrf_exempt
-def new_tweets(request):
-    """
-    Get and analyze new tweets
-    """
-
-    twitter_api = twitter.TwitterAPI("air quality")
-
-    if request.method == 'GET':
-        max_items = request.GET.get('max_items') or _DEFAULT_MAX_ITEMS
-
-        try:
-            latest_tweet = models.Sentiment.objects.filter(is_tweet=True).latest('created')
-            tweet_id = latest_tweet.tweet_id
-            tweets = twitter_api.retrieve_new(tweet_id, max_items)
-        except ObjectDoesNotExist:
-            tweets = twitter_api.retrieve(max_items)
-
-        # Serialize
-        deserializer = models.SentimentSerializer()
-
-        tweet_objs = []
-        for idx, tweet_data in enumerate(tweets):
-            tweet = deserializer.create(tweet_data)
-            tweet.is_tweet = True
-            tweet.save()
-            tweet_objs.append(tweet)
-
-        serialized = models.SentimentSerializer(tweet_objs, many=True)
-
-        return JSONResponse(serialized.data)
-
     return JSONResponse([], status=400)
